c70224190b2103288880f719cf2c2369f5e50195
Dean Koster <DeanJKoster@gmail.com>
2015-01-06 23:23:35 +0100

    changed test to not working

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,6 +1,3 @@
-/**
- * Created by rik on 12/9/14.
- */
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,12 +6,11 @@ public class TestMain {
     @Test
     public void testPasses() {
         // expected outcome
-        int expected = 5;
-        int output = Main.reken(2,3);
+        int expected = 3;
+        int output = Main.calc(2,2);
         //check if output equals expected.
         assertEquals(output, expected);
     }
-
 
 }
 
