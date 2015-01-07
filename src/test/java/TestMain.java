e5c5ec130f4b935bd256bb51ff729403d1996b16
Dean Koster <DeanJKoster@gmail.com>
2015-01-07 12:21:28 +0100

    changed test

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -6,8 +6,8 @@ public class TestMain {
     @Test
     public void testPasses() {
         // expected outcome
-        int expected = 3;
-        int output = Main.calc(1,2);
+        int expected = 2;
+        int output = Main.calc(1,1);
         //check if output equals expected.
         assertEquals(output, expected);
     }
